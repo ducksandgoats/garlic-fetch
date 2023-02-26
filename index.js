2b7c526c68c8b6a6ed53197f294aaef2aa649009
ducksandgoats <mrtraderjoseph@gmail.com>
2023-02-26 02:37:59 -0600

    update the package

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ module.exports = async function makeGarlicFetch (opts = {}) {
   const { default: nodeFetch } = await import('node-fetch')
   const detect = require('detect-port')
   const HttpProxyAgent = require('http-proxy-agent').HttpProxyAgent
-  const HttpsProxyAgent = require('https-proxy-agent').HttpsProxyAgent
   const finalOpts = { timeout: 30000, ...opts }
-  const mainConfig = {ip: '127.0.0.1', port: finalOpts.http || 4444, ports: finalOpts.https || 4445}
+  const mainPort = finalOpts.http || 4444
   const useTimeOut = finalOpts.timeout
-  const mainAgents = { 'http': new HttpProxyAgent(`http://${mainConfig.ip}:${mainConfig.port}`), 'https': new HttpsProxyAgent(`http://${mainConfig.ip}:${mainConfig.ports}`) }
+  const mainAgent = new HttpProxyAgent(`http://127.0.0.1:${mainPort}`)
 
   function handleEmpty(request) {
     const { url, headers: reqHeaders, method, body, signal } = request
@@ -54,11 +53,9 @@ module.exports = async function makeGarlicFetch (opts = {}) {
   }
 
 function useAgent(_parsedURL) {
-		if (_parsedURL.protocol === 'http:') {
-			return mainAgents.http;
-		} else if(_parsedURL.protocol === 'https:'){
-			return mainAgents.https;
-    } else {
+  if (_parsedURL.protocol === 'http:' || _parsedURL.protocol === 'https:') {
+      return mainAgent
+		} else {
       throw new Error('protocol is not valid')
     }
 	}
@@ -72,9 +69,8 @@ function useAgent(_parsedURL) {
       const mainURL = new URL(url)
 
       if(mainURL.hostname === '_'){
-        const detectedPort = await detect(mainConfig.port)
-        const detectedPorts = await detect(mainConfig.ports)
-        const isItRunning = mainConfig.port !== detectedPort && mainConfig.ports !== detectedPorts
+        const detectedPort = await detect(mainPort)
+        const isItRunning = mainPort !== detectedPort
         return {status: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, body: [String(isItRunning)]}
     }
     
@@ -85,30 +81,9 @@ function useAgent(_parsedURL) {
     
     return sendTheData(signal, await handleData(mainTimeout, nodeFetch(useLink, request)))
   }
-  async function handleIips(request) {
-    const { url, method, headers: reqHeaders, body, signal, referrer } = request
 
-    if(signal){
-      signal.addEventListener('abort', takeCareOfIt)
-    }
-      const mainURL = new URL(url)
-
-      if(mainURL.hostname === '_'){
-        const detectedPort = await detect(mainConfig.port)
-        const detectedPorts = await detect(mainConfig.ports)
-        const isItRunning = mainConfig.port !== detectedPort && mainConfig.ports !== detectedPorts
-        return {status: 200, headers: {'Content-Type': 'text/plain; charset=utf-8'}, body: [String(isItRunning)]}
-      }
-
-    request.agent = useAgent
-    const useLink = request.url.replace('iip', 'http')
-    delete request.url
-    const mainTimeout = (request.headers['x-timer'] && request.headers['x-timer'] !== '0') || (mainURL.searchParams.has('x-timer') && mainURL.searchParams.get('x-timer') !== '0') ? Number(request.headers['x-timer'] || mainURL.searchParams.get('x-timer')) * 1000 : useTimeOut
-
-    return sendTheData(signal, await Promise.race([nodeFetch(useLink, request), new Promise((resolve, reject) => setTimeout(() => {reject(new Error('timeout'))}, mainTimeout))]))
-  }
   router.any('iip://*/**', handleIip)
-  router.any('iips://*/**', handleIips)
+  router.any('iips://*/**', handleIip)
 
   return fetch
 }
